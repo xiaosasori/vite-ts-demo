419f8be249ae1bb30b7ae4e96eaef277ac8aff39
Nguyen Minh Anh <xiaosasori@gmail.com>
2021-04-28 23:39:03 +0900

    first init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <Timeline/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Timeline from './views/Timeline.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Timeline
+  }
+})
+</script>
+
+<style>
+
+</style>
